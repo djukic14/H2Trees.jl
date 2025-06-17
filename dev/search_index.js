6e56e35ea5c8c88fd90c1ecff5603ca518c30537
Documenter.jl <documenter@juliadocs.github.io>
2025-06-17 07:57:30 +0000

    build based on 62bbc4a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#H2Trees","page":"Home","title":"H2Trees","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for H2Trees.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
